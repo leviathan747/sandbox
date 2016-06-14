be32c63fccec8b9c540165bd4f8118c57654ee7a
Levi Starrett <levi@roxsoftware.com>
2016-06-14 16:59:32 -0400

    added pre-builder output and build script

---
+++ mc_java_test/gen/build.sh
@@ -0,0 +1,2 @@
+rm -rf code_generation/
+/media/psf/Home/xtuml/BridgePoint_5.3.5/BridgePoint/eclipse/plugins/org.xtuml.bp.mc.c.source_5.3.5/mc3020/bin/xtumlmc_build -home /media/psf/Home/xtuml/BridgePoint_5.3.5/BridgePoint/eclipse/plugins/org.xtuml.bp.mc.c.source_5.3.5/ -l3s -e -d code_generation -O ../../src/
