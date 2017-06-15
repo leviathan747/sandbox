1dc1e9139761472ac9143caccc3669ab73ba79da
Levi Starrett <levi@roxsoftware.com>
2017-06-15 14:57:37 -0400

    job #9538 updated model for test

--- test_ipc/gen/STRING_bridge.c
+++ test_ipc/gen/STRING_bridge.c
@@ -17,6 +17,8 @@
 #include "STRING_bridge.h"
 #include "test_ipc_sys_types.h"
 
+#include <string.h>
+
 /*
  * Bridge:  itoa
  */
@@ -71,10 +73,21 @@ STRING_itoa( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_i )
 i_t
 STRING_atoi( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
 {
-  /* Replace/Insert the following instructions with your implementation code.  */
-  /* RETURN 0 */
-  {i_t xtumlOALrv = 0;
-  return xtumlOALrv;}
+  i_t sum = 0;
+  i_t factor = 1;
+  c_t * character = p_s + strlen(p_s) - 1;
+  while ( character >= p_s ) {
+    if ( *character <= '9' && *character >= 0 ) {
+      sum += factor * (*character - '0');
+      factor *= 10;
+      character--;
+    }
+    else {
+      // error (non-integer character)
+      return -1;
+    }
+  }
+  return sum;
 }
 
 
@@ -84,9 +97,40 @@ STRING_atoi( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
 c_t *
 STRING_substr( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_begin, const i_t p_end, c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
 {
-  c_t * result = 0;
-  /* Insert your implementation code here... */
-  return result;
+  // get length of s
+  i_t len = (i_t)strlen( p_s );
+  A0xtumlsret[0] = '\0';
+
+  // check that the indexes are in a valid range
+  i_t begin = p_begin;
+  i_t end = p_end;
+  if ( begin > len - 1 ) {
+    return A0xtumlsret;
+  }
+  if ( begin < 0 ) {
+    begin = 0;
+  }
+  if ( end < 0 || end > len ) {
+    end = len;
+  }
+  if ( end <= begin ) {
+    return A0xtumlsret;
+  }
+
+  // if we have a string and the end is greater than begin
+  if ( !(len == 0 || end <= begin) ) {
+
+    // copy in the new string
+    c_t * p = p_s + begin;
+    c_t * r = A0xtumlsret;
+    while ( p < p_s + end ) {
+      *r++ = *p++;
+    }
+    *r = '\0';  // null terminate
+
+  }
+
+  return A0xtumlsret;
 }
 
 
@@ -107,9 +151,40 @@ STRING_strlen( c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
 i_t
 STRING_indexof( c_t p_haystack[ESCHER_SYS_MAX_STRING_LEN], c_t p_needle[ESCHER_SYS_MAX_STRING_LEN] )
 {
-  i_t result = 0;
-  /* Insert your implementation code here... */
-  return result;
+  c_t * a;
+  c_t * b;
+  c_t * c;
+
+  // check arguments
+  if ( !p_needle || !p_haystack ) return -1;
+
+  a = p_haystack;
+  b = p_needle;
+  if (*b == '\0') {
+    return 0;   // if needle is empty string, by definition, the index is 0
+  }
+
+  // search through to find first character match
+  for ( ; *a != 0; a += 1) {
+    if (*a == *b) {
+
+      // check the rest of the string
+      c = a;
+      while ( *c++ == *b++ ) {
+        if ( *b == '\0' ) {
+          return a - p_haystack;
+        }
+        if ( *c == '\0' ) {
+          break;
+        }
+      }
+    }
+    // reset b
+    b = p_needle;
+  }
+
+  // no match found
+  return -1;
 }
 
 
@@ -131,9 +206,30 @@ STRING_getword( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], const i_t p_i, const
 c_t *
 STRING_trim( c_t A0xtumlsret[ESCHER_SYS_MAX_STRING_LEN], c_t p_s[ESCHER_SYS_MAX_STRING_LEN] )
 {
-  c_t * result = 0;
-  /* Insert your implementation code here... */
-  return result;
+  i_t len = strlen( p_s );
+  A0xtumlsret[0] = '\0';
+
+  c_t * a;
+  c_t * b;
+
+  // find the first non whitespace character
+  a = p_s;
+  for ( ; *a != '\0'; a++ ) {
+    if ( *a != ' ' && *a != '\r' && *a != '\t' && *a != '\n' ) break;   // found non whitespace
+  }
+
+  // find last non whitespace character
+  b = p_s + ( len - 1 );
+  for ( ; b != p_s; b-- ) {
+    if ( *a != ' ' && *a != '\r' && *a != '\t' && *a != '\n' ) break;   // found non whitespace
+  }
+
+  // check if they crossed ( all whitespace )
+  if ( b >= a ) {
+    STRING_substr( A0xtumlsret, (const i_t)(a - p_s), (const i_t)(b - p_s)+1, p_s );
+  }
+
+  return A0xtumlsret;
 }
 
 
