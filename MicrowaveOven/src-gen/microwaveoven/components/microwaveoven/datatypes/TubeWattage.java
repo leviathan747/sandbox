b8c2dfa14cdf12184b6b91b68d9914909132867a
Levi Starrett <levi@roxsoftware.com>
2018-02-19 11:09:39 -0500

    removed mo

--- MicrowaveOven/src-gen/microwaveoven/components/microwaveoven/datatypes/TubeWattage.java
+++
@@ -1,4 +0,0 @@
-package microwaveoven.components.microwaveoven.datatypes;
-public enum TubeWattage {
-    LOW, MED_LOW, MEDIUM, MED_HIGH, HIGH
-}
--- MicrowaveOven/src-gen/microwaveoven/components/microwaveoven/externalentities/CP.java
+++
@@ -1,7 +0,0 @@
-package microwaveoven.components.microwaveoven.externalentities;
-
-import microwaveoven.components.microwaveoven.MicrowaveOven;
-
-public class CP {
-
-}
