8ab1bd20afc6d25d6039aa9b57e20cdba8e22bdd
Levi Starrett <levi@roxsoftware.com>
2018-05-17 10:29:29 -0400

    added realized EE

---
+++ verifier_test/javasrc/lib/EE1.java
@@ -0,0 +1,11 @@
+package lib;
+
+import org.xtuml.bp.core.CorePlugin;
+
+public class EE1 {
+	
+	public static void bar( String string_param ) {
+		CorePlugin.out.println( "Inside realized 'bar'." );
+	}
+
+}
